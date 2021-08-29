2e5e401d5cb55938e87c7322c72aac4fc0ed9edc
Serhiy Korytskyy <korysergey55@gmail.com>
2021-08-29 16:00:45 +0300

    mobile-menu

---
+++ js/mobileMenu.js
@@ -0,0 +1,14 @@
+const mobileMenuButton = document.querySelector(".mobile-menu-icon");
+const headerMenu = document.querySelector(".mobile-menu");
+
+mobileMenuButton.addEventListener("click", openMenu);
+
+function openMenu() {
+    const isOpen = headerMenu.style.display;
+  if(!isOpen || isOpen === 'none'){
+    headerMenu.style.display = "block"
+  }
+  if(isOpen === 'block'){
+    headerMenu.style.display = "none"
+  }
+}
